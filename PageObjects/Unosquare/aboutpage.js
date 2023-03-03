25f0d31ac6d1d37b48896e6d5b9535ee055a0fb6
Bruno Vasquez <bruno.vasquez@unosquare.com>
2023-03-03 13:18:41 -0400

    Amazon and Unosquare TC using data form json file

---
+++ PageObjects/Unosquare/aboutpage.js
@@ -0,0 +1,18 @@
+module.exports = {
+    url: 'https://www.unosquare.com/about-us/',
+    elements: {
+      diVece: {
+        selector: "(//h3[@class='elementor-image-box-title'])[1]", locateStrategy: 'xpath'
+      },  
+      eduardo: {
+        selector: "(//h3[@class='elementor-image-box-title'])[2]", locateStrategy: 'xpath'
+      },
+      miranda: {
+        selector: "(//h3[@class='elementor-image-box-title'])[4]", locateStrategy: 'xpath'
+      },
+      huerta: {
+        selector: "(//h3[@class='elementor-image-box-title'])[6]", locateStrategy: 'xpath'
+        
+      }
+    }
+};
